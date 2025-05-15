653d52f59c23a4fffd0755560e8c5de1650c3ccb
TechQuery <shiy2008@gmail.com>
2025-05-16 06:33:50 +0800

    [migrate] upgrade to Node.js 22, PNPM 10 & Strapi 5 [migrate] replace Azure Blob uploader with AWS S3 + CloudFlare R2 [remove] Transformer plugin & ESLint configuration
    Signed-off-by: TechQuery <shiy2008@gmail.com>

--- config/env/production/database.js
+++
@@ -1,8 +0,0 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: parse(env("DATABASE_URL")),
-  },
-});
